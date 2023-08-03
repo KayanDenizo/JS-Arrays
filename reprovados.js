f9526013f0754ff0ee8b9b028e071e923ae87654
Kayan Denizo <kayan.denizo@gmail.com>
2023-08-03 09:56:39 -0300

    Segundo Commit

---
+++ reprovados.js
@@ -0,0 +1,9 @@
+const alunos = ['Ana', 'Marcos', 'Roberto', 'Mauro'];
+const medias = [7, 4.5, 8, 7.5];
+
+const reprovados = alunos.filter((_, indice)=> {
+  return medias[indice] < 7; //True ou False
+});
+
+
+console.log(reprovados);
